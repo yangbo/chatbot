a4814aded2f570222222936aa26ef312d1e68311
bo.yang <bo.yang@telecwin.com>
2019-09-19 11:11:27 +0800

    解决环境变量问题

---
+++ seq2seqChatbot/set_cuda.bat
@@ -0,0 +1,7 @@
+set CUDA_PATH=d:\dl\cuda\v8.0\
+rem set PATH=C:\WINDOWS\System32\WindowsPowerShell\v1.0\
+rem set PATH=%CUDA_PATH%\bin;
+rem set PATH=%CUDA_PATH%\lib;%PATH%
+rem set PATH=%CUDA_PATH%\include;%PATH%
+rem set PATH=%CUDA_PATH%;%PATH%
+Path=d:\dl\cuda\v8.0\bin;d:\dl\cuda\v8.0\libnvvp;D:\dl\cuda\cudnn-8.0-windows10-x64-v6\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;E:\program\Git\cmd;e:\java\gradle-4.9\\bin;F:\js\nodejs10\;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;D:\dl\cuda\v8.0\extras\CUPTI\libx64;C:\Program Files\NVIDIA Corporation\NVIDIA NvDLISR;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Go\bin;E:\program\yarn\bin\;c:\Program Files\NVIDIA Corporation\NVSMI\;C:\Users\yangbo\AppData\Local\Microsoft\WindowsApps;C:\Users\yangbo\AppData\Roaming\npm;E:\program\Microsoft VS Code\bin;d:\dl\anaconda2\Scripts\;f:\program\GnuWin32\bin\;E:\program\WebStorm 2018.3.5\bin;C:\Users\yangbo\go\bin;C:\Users\yangbo\AppData\Local\Yarn\bin
